52d6a0b81a3f1e52a45d6711507ea316cb129233
Alejandra Cabrera <60221911+AlejanCa@users.noreply.github.com>
2020-02-02 03:37:03 -0300

    Add files via upload

---
+++ formulario de registro/validar.js
@@ -0,0 +1,21 @@
+function validar(){
+    var nombre,apellidos,correo,usuario,clave,telefono,expresion;
+    nombre=document.getElementById("nombre").value;
+    apellidos= document.getElementById("apellidos").value;
+    correo= document.getElementById("correo").value;
+    usuario= document.getElementById("usuario").value;
+    clave= document.getElementById("clave").value;
+    telefono= document.getElementById("telefono").value;
+
+    if(nombre===""||apellidos===""||correo===""||usuario===""||clave===""||telefono==="")
+    {alert("Todos los campos son obligatorios");
+    return false;
+}
+else if(nombre.length>30){
+    alert("el nombre es muy largo");
+    
+    
+    return false;
+}
+    
+}
